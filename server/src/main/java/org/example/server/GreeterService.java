e74ff036cf6081706e6893e3c16ff25b2143c043
Nhahan <kisy324@naver.com>
2024-11-14 16:45:47 +0900

    feat: `server` module

---
+++ server/src/main/java/org/example/server/GreeterService.java
@@ -0,0 +1,19 @@
+package org.example.server;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.example.common.GreeterGrpc;
+import org.example.common.HelloReply;
+import org.example.common.HelloRequest;
+import io.grpc.stub.StreamObserver;
+
+@GrpcService
+public class GreeterService extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        String message = "Hello, " + request.getName();
+        HelloReply reply = HelloReply.newBuilder().setMessage(message).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
